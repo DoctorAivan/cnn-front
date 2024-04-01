92e54f4b7ed1a78f956fc95bc54774be8a8997f1
DoctorAivan <design.villarroel.s@gmail.com>
2024-04-01 17:42:26 -0300

    UI Updates

--- cnn.js
+++ cnn.js
@@ -6,31 +6,34 @@ const main = document.querySelector("#main")
 const live = document.querySelector("#live")
 const footer = document.querySelector("#footer")
 
-let device_type = 'desktop'
-
-//  Configuración Live
-let live_view_status = false
+// Skyscraper Contents
+const skyscraper_left = document.querySelector("#skyscraper-left")
+const skyscraper_right = document.querySelector("#skyscraper-right")
+const skyscraper_initial_y = skyscraper_left.offsetTop - 70
 
 //  Mobile Menu
 const mobile_menu = document.querySelector("#mobile-menu")
 const mobile_menu_content = document.querySelector("#mobile-menu-content")
 const mobile_menu_button = document.querySelector("#mobile-menu-button")
 const mobile_menu_button_icon = document.querySelector("#mobile-menu-button > i")
-
-let mobile_menu_status = false
-
 const desktop_search_ui = document.querySelector("#desktop-search-ui")
 const desktop_search_ui_button = document.querySelector("#desktop-search-ui-button")
+
+// Device Default Type
+let device_type = 'desktop'
+
+//  Configuración Live
+let live_view_status = false
+let mobile_menu_status = false
 let desktop_search_ui_status = false
 
+// Search UI
 const seach_ui = () => {
-    if( desktop_search_ui_status )
-    {
+    if (desktop_search_ui_status) {
         desktop_search_ui.style.display = 'none'
         desktop_search_ui_status = false
     }
-    else
-    {
+    else {
         desktop_search_ui.style.display = 'grid'
         desktop_search_ui_status = true
     }
@@ -41,6 +44,7 @@ mobile_menu_button.addEventListener('click', (event) => {
 
     //  Validate menu status
     if (mobile_menu_status) {
+
         //  Change display status
         mobile_menu_status = false
         mobile_menu_content.style.display = "none"
@@ -54,6 +58,7 @@ mobile_menu_button.addEventListener('click', (event) => {
 
     }
     else {
+
         //  Change display status
         mobile_menu_status = true
         mobile_menu_content.style.display = "block"
@@ -77,57 +82,41 @@ const getDevice = () => {
     else {
         device_type = 'desktop'
     }
-
-    let device = navigator.userAgent
-
-    if (/Android/i.test(device)) {
-        console.log('Other');
-        header.style.height = '60px'
-        header.style.padding = '10px 0px 0px 0px'
-
-        main.style.margin = '60px 0px 0px 0px'
-    }
 }
 
+// Status On Air Player
 const liveStatus = () => {
 
     //  Detener Live
     if (live_view_status) {
-
         player.stopVideo();
-        
+
         setTimeout(() => {
             live.style.display = 'block'
 
-        //  Detect Device
-            if( device_type == 'desktop' )
-            {
+            //  Detect Device
+            if (device_type == 'desktop') {
                 live.classList.remove('animate-liveIn')
                 live.classList.add('animate-liveOut')
             }
-            else
-            {
+            else {
                 live.classList.remove('animate-liveInMobile')
                 live.classList.add('animate-liveOutMobile')
             }
-    
+
             //  Change Live status
             live_view_status = false
         }, 500)
     }
-
-    //  Iniciar Live
     else {
         window.scrollTo({ top: 0, behavior: 'smooth' });
 
         //  Detect Device
-        if( device_type == 'desktop' )
-        {
+        if (device_type == 'desktop') {
             live.classList.remove('animate-liveOut')
             live.classList.add('animate-liveIn')
         }
-        else
-        {
+        else {
             live.classList.remove('animate-liveOutMobile')
             live.classList.add('animate-liveInMobile')
         }
@@ -144,43 +133,40 @@ const liveStatus = () => {
     }
 }
 
-const skyscraper_left = document.querySelector("#skyscraper-left")
-const skyscraper_right = document.querySelector("#skyscraper-right")
-
+// Listener in Scroll
 window.addEventListener('scroll', (e) => {
 
+    // Default Values
     let scroll_limit
     let scroll_y = document.documentElement.scrollTop
 
-    if(live_view_status)
-    {
-        scroll_limit = 508 + 158
+    // Validate On Air Satus
+    if (live_view_status) {
+        scroll_limit = 508 + skyscraper_initial_y
     }
-    else
-    {
-        scroll_limit = 158
+    else {
+        scroll_limit = skyscraper_initial_y
     }
 
-    if(scroll_y > scroll_limit)
-    {
+    // Validate Scroll Position
+    if (scroll_y > scroll_limit) {
         skyscraper_left.style.position = 'fixed'
         skyscraper_left.style.top = '70px'
         skyscraper_right.style.position = 'fixed'
         skyscraper_right.style.top = '70px'
     }
-    else
-    {
+    else {
         skyscraper_left.style.position = 'absolute'
         skyscraper_left.style.top = 'auto'
         skyscraper_right.style.position = 'absolute'
         skyscraper_right.style.top = 'auto'
     }
-
 })
 
 //  Document Complete
 window.addEventListener('DOMContentLoaded', (event) => {
 
+    // Get Device Data
     getDevice();
 
 });
@@ -196,5 +182,6 @@ function onYouTubePlayerAPIReady() {
 // Inject YouTube API script
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
+
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
